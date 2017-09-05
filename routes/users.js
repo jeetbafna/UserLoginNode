62f00afd0bec18b3d25eb99f67cd8e1848c197c8
jeet bafna <jeetanil97@gmail.com>
2017-09-05 23:11:14 +0530

    added validation

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest : './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,5 +13,39 @@ router.get('/register', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login',{title: 'Login'});
 });
+router.post('/register', upload.single('profileimage'),function(req, res, next) {
+	var name =req.body.name;
+	var email =req.body.email;
+	var username =req.body.username;
+	var password =req.body.password;
+	var password2 =req.body.password2;
+	if(req.file){
+		console.log('Uploading file');
+		var profileimage = req.file.filename;
+	} else{
+		console.log('no file uploaded');
+		var profileimage = 'noimage.jpeg';
+	}
 
+	//FOrm Validation
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('email','Email field is required').notEmpty();
+	req.checkBody('email','Email is not valid').isEmail();
+	req.checkBody('username','UserName field is required').notEmpty();
+	req.checkBody('password','Password field is required').notEmpty();
+	req.checkBody('password2','Passwords do not match').equals(req.body.password);
+
+
+
+	//CHeck Errors
+	var errors = req.validationErrors();
+	if(errors){
+		res.render('register',{
+			errors: errors
+		});
+	}
+	else{
+		console.log('No Errors');
+	}
+});
 module.exports = router;
